0f630a20d2dd450d388da5adc5d33b1495c24d3a
Igor Goldvekht <goldvekht.igor@gmail.com>
2016-10-12 18:53:15 -0400

    Implement application of Gaussian Filter

--- Filters.hpp
+++ Filters.hpp
@@ -38,4 +38,32 @@ vector< vector<double> > createGaussian(int size, double sigma) {
   return kernel;
 }
 
+int getGaussianConvolution(Mat &image, int x, int y, vector< vector<double> > gKernel) {
+  int value = 0;
+  int halfSize = gKernel.size()/2;
+
+  for (int i = -halfSize; i <= halfSize; i++) {
+    for (int j = -halfSize; j <= halfSize; j++) {
+      value += gKernel[i+halfSize][j+halfSize] * image.at<uchar>(x + i, y + j);
+    }
+  }
+
+  return value;
+}
+
+void applyGaussian(Mat &src, Mat &dest, vector< vector<double> > gKernel) {
+  dest.create(src.size(), src.type());
+
+  int margin = gKernel.size()/2;
+  for (int i = margin; i < src.rows - margin; i++) {
+    for (int j = margin; j < src.cols - margin; j++) {
+      /* if (src.at<uchar>(i, j) > 140) src.at<uchar>(i,j) = 255; */
+      /* else src.at<uchar>(i,j) = 0; */
+
+      int intensity = getGaussianConvolution(src, i, j, gKernel);
+      dest.at<uchar>(i, j) = intensity;
+    }
+  }
+}
+
 #endif
